f0cfd5605a8f590fd85892895e832091ba0b5bb5
Jacob Arriola <jarriola2012@gmail.com>
2019-02-13 12:53:48 -0800

    Updated website stuff

--- hello.py
+++ hello.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def home():
 	return render_template('layout.html')
 
-@app.route('/about/')
-def about():
-	return render_template('layout.html')
-
 if __name__ == '__main__':
 	app.run(debug=True)
